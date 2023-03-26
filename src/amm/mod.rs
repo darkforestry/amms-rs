3ca1df972a6cd0dec717345205b5a4771749ca7b
0xKitsune <0xKitsune@protonmail.com>
2023-03-26 15:39:38 -0400

    added rough outline for amm trait

--- src/amm/mod.rs
+++ src/amm/mod.rs
@@ -1,1 +1,13 @@
-pub trait AutomatedMarketMaker {} //TODO: maybe shorten this to AMM instead
+pub trait AutomatedMarketMaker {
+    //address()
+    //calculate_price
+    // simulate_swap
+    // stimulate_swap_mut
+    // swap_calldata
+    // tokens -> vec<token> (optional)
+    // sync()
+    // sync from log()
+    // sync events (triggers to listen for that will cause to sync or something)
+}
+
+pub enum AMM {}
