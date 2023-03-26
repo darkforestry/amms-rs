e063cf5e83982ea866b4189cf7ffe204dd6c358e
0xKitsune <0xKitsune@protonmail.com>
2023-03-26 15:25:19 -0400

    reworking the lib

---
+++ src/amm/mod.rs
@@ -0,0 +1,1 @@
+pub trait AutomatedMarketMaker {} //TODO: maybe shorten this to AMM instead
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,1 @@
-mod abi;
-pub mod checkpoint;
-pub mod dex;
-pub mod errors;
-pub mod pool;
-pub mod sync;
-pub mod throttle;
-pub use pool::simulate_route;
-pub use pool::simulate_route_mut;
-pub mod batch_requests;
+pub mod amm;
