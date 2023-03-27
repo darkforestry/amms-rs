18693f887bd2fca5842d04a1d3373446beebed5e
0xKitsune <0xKitsune@protonmail.com>
2023-03-26 23:10:43 -0400

    updated amm function signature order

--- src/amm.rs
+++ src/amm.rs
@@ -14,13 +14,13 @@ use crate::{
 #[async_trait]
 pub trait AutomatedMarketMaker {
     fn address(&self) -> H160;
-    fn calculate_price(&self, base_token: H160) -> Result<f64, ArithmeticError>;
-    fn simulate_swap(&self, token_in: H160, amount_in: U256, token_out: H160) -> U256;
-    fn simulate_swap_mut(&mut self, token_in: H160, amount_in: U256, token_out: H160) -> U256;
-    fn tokens(&self) -> Vec<H160>;
     async fn sync<M: Middleware>(&mut self, middleware: Arc<M>) -> Result<(), DAMMError<M>>;
     async fn sync_from_log(&mut self, log: &Log);
     fn sync_on_events(&self) -> Vec<H256>;
+    fn tokens(&self) -> Vec<H160>;
+    fn calculate_price(&self, base_token: H160) -> Result<f64, ArithmeticError>;
+    fn simulate_swap(&self, token_in: H160, amount_in: U256, token_out: H160) -> U256;
+    fn simulate_swap_mut(&mut self, token_in: H160, amount_in: U256, token_out: H160) -> U256;
 }
 
 pub enum AMM {
