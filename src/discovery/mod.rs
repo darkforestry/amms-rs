55ab401431c251f714efb8a6e7903f63cba29ee5
0xKitsune <0xKitsune@protonmail.com>
2023-03-31 12:20:15 -0400

    added mod for interface_check, added discoverytargets

--- src/discovery/mod.rs
+++ src/discovery/mod.rs
@@ -1,1 +1,27 @@
+pub mod interface_check;
 
+use ethers::types::H256;
+
+use crate::amm;
+
+pub enum DiscoveryTarget {
+    UniswapV2Factory,
+    UniswapV3Factory,
+}
+
+impl DiscoveryTarget {
+    pub fn discovery_event_signature(&self) -> H256 {
+        match self {
+            DiscoveryTarget::UniswapV2Factory => {
+                amm::uniswap_v2::factory::PAIR_CREATED_EVENT_SIGNATURE
+            }
+
+            DiscoveryTarget::UniswapV3Factory => {
+                amm::uniswap_v3::factory::POOL_CREATED_EVENT_SIGNATURE
+            }
+        }
+    }
+}
+
+//TODO: implement a function that goes through all logs and checks if a potential factory address adheres to the factory interface
+// defined in the interface_check mod
