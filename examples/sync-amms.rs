7c57202fd213b501d7b9b5512512b92ca312f1bb
LeytonTaylor <leytontaylor23@gmail.com>
2023-04-21 19:22:15 -0700

    Debugging logs in update_tick

--- examples/sync-amms.rs
+++ examples/sync-amms.rs
@@ -18,25 +18,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
     //Add rpc endpoint here:
     let rpc_endpoint =
         std::env::var("ETHEREUM_RPC_ENDPOINT").expect("Could not get ETHEREUM_RPC_ENDPOINT");
-    let provider = Arc::new(Provider::<Http>::try_from(rpc_endpoint).unwrap());
+    let provider = Arc::new(Provider::<Http>::try_from("https://arb-mainnet.g.alchemy.com/v2/wnjMjLtVqyy-kpSYPIrPV3NzbX4azveG").unwrap());
 
     let factories = vec![
-        //UniswapV2
-        Factory::UniswapV2Factory(UniswapV2Factory::new(
-            H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f").unwrap(),
-            2638438,
-            300,
-        )),
-        //Add Sushiswap
-        Factory::UniswapV2Factory(UniswapV2Factory::new(
-            H160::from_str("0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac").unwrap(),
-            10794229,
-            300,
-        )),
+        // //UniswapV2
+        // Factory::UniswapV2Factory(UniswapV2Factory::new(
+        //     H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f").unwrap(),
+        //     2638438,
+        //     300,
+        // )),
+        // //Add Sushiswap
+        // Factory::UniswapV2Factory(UniswapV2Factory::new(
+        //     H160::from_str("0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac").unwrap(),
+        //     10794229,
+        //     300,
+        // )),
         //Add UniswapV3
         Factory::UniswapV3Factory(UniswapV3Factory::new(
             H160::from_str("0x1F98431c8aD98523631AE4a59f267346ea31F984").unwrap(),
-            12369621,
+            185,
         )),
     ];
 
