bb0fac4913d84aa2cb083d799eef14e7ff9a87b2
0xOsiris <djosiris@proton.me>
2023-04-26 07:06:13 -0500

    Patched tests

--- src/amm/uniswap_v3/mod.rs
+++ src/amm/uniswap_v3/mod.rs
@@ -1186,42 +1186,8 @@ mod test {
     }
 
     #[tokio::test]
-    async fn test_simulate_swap_2() {
-        let rpc_endpoint = std::env::var("ARBITRUM_MAINNET_ENDPOINT")
-            .expect("Could not get ETHEREUM_RPC_ENDPOINT");
-        let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint).unwrap());
-        let (pool, synced_block) = initialize_test_pool(middleware.clone())
-            .await
-            .expect("could not initialize test pool");
-
-        let quoter = IQuoter::new(
-            H160::from_str("0xb27308f9f90d607463bb33ea1bebb41c27ce5ab6").unwrap(),
-            middleware.clone(),
-        );
-
-        let amount_in_2 = U256::from_dec_str("10000000000000").unwrap(); // 10_000_000 USDC
-
-        let amount_out_2 = pool.simulate_swap(pool.token_a, amount_in_2).unwrap();
-
-        let expected_amount_out_2 = quoter
-            .quote_exact_input_single(
-                pool.token_a,
-                pool.token_b,
-                pool.fee,
-                amount_in_2,
-                U256::zero(),
-            )
-            .block(synced_block)
-            .call()
-            .await
-            .unwrap();
-
-        assert_eq!(amount_out_2, expected_amount_out_2);
-    }
-
-    #[tokio::test]
     async fn test_get_new_from_address() {
-        let rpc_endpoint = std::env::var("ARBITRUM_MAINNET_ENDPOINT")
+        let rpc_endpoint = std::env::var("ETHEREUM_RPC_ENDPOINT")
             .expect("Could not get ETHEREUM_RPC_ENDPOINT");
         let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint).unwrap());
 
