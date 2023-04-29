8d1a9e47fc929516d4fcbd7758ed9cefcef76b96
0xOsiris <djosiris@proton.me>
2023-04-29 07:03:54 -0500

    Added burn/mint events to abigen

--- src/amm/uniswap_v3/mod.rs
+++ src/amm/uniswap_v3/mod.rs
@@ -41,6 +41,8 @@ abigen!(
         function tickBitmap(int16 wordPosition) external view returns (uint256)
         function swap(address recipient, bool zeroForOne, int256 amountSpecified, uint160 sqrtPriceLimitX96, bytes calldata data) external returns (int256, int256)
         event Swap( address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)
+        event Burn(address indexed owner,int24 indexed tickLower,int24 indexed tickUpper,uint128 amount,uint256 amount0,uint256 amount1)
+        event Mint(address sender,address indexed owner,int24 indexed tickLower,int24 indexed tickUpper,uint128 amount,uint256 amount0,uint256 amount1)
     ]"#;
 
     IErc20,
