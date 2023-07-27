1c0ef7afb979fb5fa64ff780d2cb4329cdf1183f
0xOsiris <djosiris@proton.me>
2023-07-26 21:36:52 -0500

    chore: update examples

--- examples/sync-amms.rs
+++ examples/sync-amms.rs
@@ -22,11 +22,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let factories = vec![
         //UniswapV2
-        // Factory::UniswapV2Factory(UniswapV2Factory::new(
-        //     H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f").unwrap(),
-        //     2638438,
-        //     300,
-        // )),
+        Factory::UniswapV2Factory(UniswapV2Factory::new(
+            H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f").unwrap(),
+            2638438,
+            300,
+        )),
         // //Add Sushiswap
         Factory::UniswapV2Factory(UniswapV2Factory::new(
             H160::from_str("0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac").unwrap(),
@@ -34,14 +34,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
             300,
         )),
         //Add UniswapV3
-        // Factory::UniswapV3Factory(UniswapV3Factory::new(
-        //     H160::from_str("0x1F98431c8aD98523631AE4a59f267346ea31F984").unwrap(),
-        //     185,
-        // )),
-        // Factory::IziSwapFactory(IziSwapFactory::new(
-        //     H160::from_str("0x45e5f26451cdb01b0fa1f8582e0aad9a6f27c218").unwrap(),
-        //     26815159,
-        // )),
+        Factory::UniswapV3Factory(UniswapV3Factory::new(
+            H160::from_str("0x1F98431c8aD98523631AE4a59f267346ea31F984").unwrap(),
+            185,
+        )),
     ];
 
     //Sync pairs
