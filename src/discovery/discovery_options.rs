09037a4287c9f8671c326254d4551dbd2d788dd6
Nguyen Le Quang <quangkeu95@gmail.com>
2023-09-30 00:25:07 +0700

    fix(discovery): Update default start block from FactoriesDiscoveryOption

--- src/discovery/discovery_options.rs
+++ src/discovery/discovery_options.rs
@@ -3,8 +3,8 @@ use derive_builder::Builder;
 /// Factories discovery options
 #[derive(Debug, Builder, Default)]
 pub struct FactoriesDiscoveryOptions {
-    /// From block number, if None then the discovery start block number will be 0
-    #[builder(default = "0")]
+    /// From block number, if None then the discovery start block number will be 10000835 (the creation block of UniswapV2Factory)
+    #[builder(default = "10000835")]
     pub from_block: u64,
     /// To block number, if None then the discovery end block number will be the current block number
     #[builder(default)]
@@ -13,7 +13,6 @@ pub struct FactoriesDiscoveryOptions {
     #[builder(default = "1000")]
     pub step: u64,
     /// Filter factory that have to has at least a number of pairs
-    #[builder(default = "10")]
     pub number_of_amms_threshold: u64,
 }
 
