24c063663bbef8371e9866109ca4ea42878914fe
0xDmtri <0xDmtri@protonmail.com>
2024-04-28 18:33:37 +0100

    refactor to alloy

--- src/amm/mod.rs
+++ src/amm/mod.rs
@@ -5,11 +5,13 @@ pub mod uniswap_v3;
 
 use std::sync::Arc;
 
-use async_trait::async_trait;
-use ethers::{
-    providers::Middleware,
-    types::{Log, H160, H256, U256},
+use alloy::{
+    primitives::{Address, B256, U256},
+    providers::{network::AnyNetwork, Provider},
+    rpc::types::eth::Log,
+    transports::Transport,
 };
+use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 
 use crate::errors::{AMMError, ArithmeticError, EventLogError, SwapSimulationError};
@@ -19,46 +21,53 @@ use self::{erc_4626::ERC4626Vault, uniswap_v2::UniswapV2Pool, uniswap_v3::Uniswa
 #[async_trait]
 pub trait AutomatedMarketMaker {
     /// Returns the address of the AMM.
-    fn address(&self) -> H160;
+    fn address(&self) -> Address;
 
     /// Syncs the AMM data on chain via batched static calls.
-    async fn sync<M: Middleware>(&mut self, middleware: Arc<M>) -> Result<(), AMMError<M>>;
+    async fn sync<T: Transport + Clone, P: Provider<T, AnyNetwork>>(
+        &mut self,
+        provider: Arc<P>,
+    ) -> Result<(), AMMError>;
 
     /// Returns the vector of event signatures subscribed to when syncing the AMM.
-    fn sync_on_event_signatures(&self) -> Vec<H256>;
+    fn sync_on_event_signatures(&self) -> Vec<B256>;
 
     /// Returns a vector of tokens in the AMM.
-    fn tokens(&self) -> Vec<H160>;
+    fn tokens(&self) -> Vec<Address>;
 
     /// Calculates a f64 representation of base token price in the AMM.
-    fn calculate_price(&self, base_token: H160) -> Result<f64, ArithmeticError>;
+    fn calculate_price(&self, base_token: Address) -> Result<f64, ArithmeticError>;
 
     /// Updates the AMM data from a log.
     fn sync_from_log(&mut self, log: Log) -> Result<(), EventLogError>;
 
     /// Populates the AMM data via batched static calls.
-    async fn populate_data<M: Middleware>(
+    async fn populate_data<T: Transport + Clone, P: Provider<T, AnyNetwork>>(
         &mut self,
         block_number: Option<u64>,
-        middleware: Arc<M>,
-    ) -> Result<(), AMMError<M>>;
+        middleware: Arc<P>,
+    ) -> Result<(), AMMError>;
 
     /// Locally simulates a swap in the AMM.
     ///
     /// Returns the amount received for `amount_in` of `token_in`.
-    fn simulate_swap(&self, token_in: H160, amount_in: U256) -> Result<U256, SwapSimulationError>;
+    fn simulate_swap(
+        &self,
+        token_in: Address,
+        amount_in: U256,
+    ) -> Result<U256, SwapSimulationError>;
 
     /// Locally simulates a swap in the AMM.
     /// Mutates the AMM state to the state of the AMM after swapping.
     /// Returns the amount received for `amount_in` of `token_in`.
     fn simulate_swap_mut(
         &mut self,
-        token_in: H160,
+        token_in: Address,
         amount_in: U256,
     ) -> Result<U256, SwapSimulationError>;
 
     /// Returns the token out of the AMM for a given `token_in`.
-    fn get_token_out(&self, token_in: H160) -> H160;
+    fn get_token_out(&self, token_in: Address) -> Address;
 }
 
 macro_rules! amm {
@@ -70,19 +79,19 @@ macro_rules! amm {
 
         #[async_trait]
         impl AutomatedMarketMaker for AMM {
-            fn address(&self) -> H160 {
+            fn address(&self) -> Address{
                 match self {
                     $(AMM::$pool_type(pool) => pool.address(),)+
                 }
             }
 
-            async fn sync<M: Middleware>(&mut self, middleware: Arc<M>) -> Result<(), AMMError<M>> {
+            async fn sync<T: Transport + Clone, P: Provider<T, AnyNetwork>>(&mut self, middleware: Arc<P>) -> Result<(), AMMError> {
                 match self {
                     $(AMM::$pool_type(pool) => pool.sync(middleware).await,)+
                 }
             }
 
-            fn sync_on_event_signatures(&self) -> Vec<H256> {
+            fn sync_on_event_signatures(&self) -> Vec<B256> {
                 match self {
                     $(AMM::$pool_type(pool) => pool.sync_on_event_signatures(),)+
                 }
@@ -94,37 +103,37 @@ macro_rules! amm {
                 }
             }
 
-            fn simulate_swap(&self, token_in: H160, amount_in: U256) -> Result<U256, SwapSimulationError> {
+            fn simulate_swap(&self, token_in: Address, amount_in: U256) -> Result<U256, SwapSimulationError> {
                 match self {
                     $(AMM::$pool_type(pool) => pool.simulate_swap(token_in, amount_in),)+
                 }
             }
 
-            fn simulate_swap_mut(&mut self, token_in: H160, amount_in: U256) -> Result<U256, SwapSimulationError> {
+            fn simulate_swap_mut(&mut self, token_in: Address, amount_in: U256) -> Result<U256, SwapSimulationError> {
                 match self {
                     $(AMM::$pool_type(pool) => pool.simulate_swap_mut(token_in, amount_in),)+
                 }
             }
 
-            fn get_token_out(&self, token_in: H160) -> H160 {
+            fn get_token_out(&self, token_in: Address) -> Address {
                 match self {
                     $(AMM::$pool_type(pool) => pool.get_token_out(token_in),)+
                 }
             }
 
-            async fn populate_data<M: Middleware>(&mut self, block_number: Option<u64>, middleware: Arc<M>) -> Result<(), AMMError<M>> {
+            async fn populate_data<T: Transport + Clone, P: Provider<T, AnyNetwork>>(&mut self, block_number: Option<u64>, middleware: Arc<P>) -> Result<(), AMMError> {
                 match self {
                     $(AMM::$pool_type(pool) => pool.populate_data(block_number, middleware).await,)+
                 }
             }
 
-            fn tokens(&self) -> Vec<H160> {
+            fn tokens(&self) -> Vec<Address> {
                 match self {
                     $(AMM::$pool_type(pool) => pool.tokens(),)+
                 }
             }
 
-            fn calculate_price(&self, base_token: H160) -> Result<f64, ArithmeticError> {
+            fn calculate_price(&self, base_token: Address) -> Result<f64, ArithmeticError> {
                 match self {
                     $(AMM::$pool_type(pool) => pool.calculate_price(base_token),)+
                 }
