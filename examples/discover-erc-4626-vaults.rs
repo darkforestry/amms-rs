af71dbc8f1e300d79addbdfbcac0b174047a040b
0xDmtri <0xDmtri@protonmail.com>
2024-05-02 21:30:21 +0200

    migrate 'examples' to Alloy

--- examples/discover-erc-4626-vaults.rs
+++ examples/discover-erc-4626-vaults.rs
@@ -1,21 +1,21 @@
-// use amms::discovery;
-// use ethers::providers::{Http, Provider};
-// use std::sync::Arc;
-//
-// #[tokio::main]
-// async fn main() -> eyre::Result<()> {
-//     tracing_subscriber::fmt::init();
-//
-//     //Add rpc endpoint here:
-//     let rpc_endpoint = std::env::var("ETHEREUM_RPC_ENDPOINT")?;
-//     let provider = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
-//
-//     //discover vaults
-//     let vaults = discovery::erc_4626::discover_erc_4626_vaults(provider, 30000).await?;
-//
-//     println!("Vaults: {:?}", vaults);
-//
-//     Ok(())
-// }
+use std::sync::Arc;
 
-fn main() {}
+use alloy::providers::ProviderBuilder;
+
+use amms::discovery;
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    // Add rpc endpoint here:
+    let rpc_endpoint = std::env::var("ETHEREUM_RPC_ENDPOINT")?;
+    let provider = Arc::new(ProviderBuilder::new().on_http(rpc_endpoint.parse()?));
+
+    // discover vaults
+    let vaults = discovery::erc_4626::discover_erc_4626_vaults(provider, 30000).await?;
+
+    println!("Vaults: {:?}", vaults);
+
+    Ok(())
+}
