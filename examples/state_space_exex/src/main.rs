2ab54cf299b307c34a2a6069457a28fe56aa9c5b
0xKitsune <0xKitsune@protonmail.com>
2024-05-12 14:05:51 -0400

    init exex logic

---
+++ examples/state_space_exex/src/main.rs
@@ -0,0 +1,34 @@
+async fn init_exex<Node: FullNodeComponents>(
+    ctx: ExExContext<Node>,
+) -> eyre::Result<impl Future<Output = eyre::Result<()>>> {
+    //TODO: init state space manager
+    // state_space_exex(ctx, state_space_manager)
+}
+
+fn main() -> eyre::Result<()> {
+    reth::cli::Cli::parse_args().run(|builder, _| async move {
+        let handle = builder
+            .node(EthereumNode::default())
+            .install_exex(
+                "StateSpaceManager",
+                |ctx| async move { init_exex(ctx).await },
+            )
+            .launch()
+            .await?;
+
+        handle.wait_for_node_exit().await
+    })
+}
+
+async fn state_space_exex<Node: FullNodeComponents>(
+    mut ctx: ExExContext<Node>,
+    state_space_manager: StateSpaceManager,
+) -> eyre::Result<()> {
+    while let Some(notification) = ctx.notifications.recv().await {
+        // match notification {
+
+        // }
+    }
+
+    Ok(())
+}
