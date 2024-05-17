5cdd79bdd753ff96ce60b31befa251b09468c445
0xKitsune <0xKitsune@protonmail.com>
2024-05-17 19:23:51 -0400

    bump deps

--- examples/state_space_exex/src/main.rs
+++ examples/state_space_exex/src/main.rs
@@ -7,6 +7,45 @@ use std::future::Future;
 async fn init_exex<Node: FullNodeComponents>(
     ctx: ExExContext<Node>,
 ) -> eyre::Result<impl Future<Output = eyre::Result<()>>> {
+    //TODO: load config
+
+    let ws_endpoint = std::env::var("ETHEREUM_WS_ENDPOINT")?;
+
+    // Initialize WS provider
+    let ws = WsConnect::new(ws_endpoint);
+    let provider = Arc::new(ProviderBuilder::new().on_ws(ws).await?);
+
+    // Initialize factories
+    let factories = vec![
+        // Add UniswapV2
+        Factory::UniswapV2Factory(UniswapV2Factory::new(
+            address!("5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"),
+            2638438,
+            300,
+        )),
+        // Add Sushiswap
+        Factory::UniswapV2Factory(UniswapV2Factory::new(
+            address!("C0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac"),
+            10794229,
+            300,
+        )),
+    ];
+
+    let step: u64 = 1000;
+
+    // Sync amms
+    let (mut amms, last_synced_block) =
+        sync::sync_amms(factories, provider.clone(), None, step).await?;
+
+    // Discover vaults and add them to amms
+    let vaults = discovery::erc_4626::discover_erc_4626_vaults(provider.clone(), step)
+        .await?
+        .into_iter()
+        .map(AMM::ERC4626Vault)
+        .collect::<Vec<AMM>>();
+
+    amms.extend(vaults);
+
     //TODO: init state space manager
     // state_space_exex(ctx, state_space_manager)
 }
@@ -28,12 +67,10 @@ fn main() -> eyre::Result<()> {
 
 async fn state_space_exex<Node: FullNodeComponents>(
     mut ctx: ExExContext<Node>,
-    state_space_manager: StateSpaceManager,
+    mut state_space_manager: StateSpaceManager,
 ) -> eyre::Result<()> {
     while let Some(notification) = ctx.notifications.recv().await {
-        // match notification {
-
-        // }
+        match notification {}
     }
 
     Ok(())
