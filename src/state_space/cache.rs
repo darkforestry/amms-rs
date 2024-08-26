587bc6ca0238b9ec765e1f90bba94aa71825f659
0xKitsune <0xkitsune@protonmail.com>
2024-08-26 17:57:39 -0400

    update unwind state change cache

--- src/state_space/cache.rs
+++ src/state_space/cache.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use crate::amm::{AutomatedMarketMaker, AMM};
+
 use super::StateChange;
 use arraydeque::{ArrayDeque, CapacityError};
 
@@ -17,36 +21,9 @@ impl StateChangeCache {
         }
     }
 
-    //TODO: push back
-
-    pub fn push_front(
-        &mut self,
-        state_change: StateChange,
-    ) -> Result<(), CapacityError<StateChange>> {
-        self.0.push_front(state_change)
-    }
-
-    pub fn pop_front(&mut self) -> Option<StateChange> {
-        self.cache.pop_front()
-    }
-
-    pub fn capacity(&self) -> usize {
-        self.cache.capacity()
-    }
-
-    pub fn len(&self) -> usize {
-        self.cache.len()
-    }
-
     pub fn is_empty(&self) -> bool {
         self.cache.is_empty()
     }
-
-    pub fn clear(&mut self) {
-        self.cache.clear();
-    }
-
-    // TODO: add state change to cache
 
     pub fn add_state_change_to_cache(
         &mut self,
@@ -61,37 +38,47 @@ impl StateChangeCache {
         cache.push_front(state_change)
     }
 
-    // TODO: update return type
-    async fn unwind_state_changes(&mut self, block_to_unwind: u64) -> Vec<StateChange> {
+    /// Unwinds the state changes up to the given block number
+    /// Returns the state of the affected AMMs at the block number provided
+    async fn unwind_state_changes(&mut self, block_to_unwind: u64) -> Vec<AMM> {
         let cache = &mut self.cache;
-
-        // TODO: check if the block to unwind is greater than the most recent state change block number
-        // TODO: check if the block to unwind is less than the oldest state change block number,
-        // NOTE: this needs to handle cases where cache is empty or the state changes are newer than the unwind
-        // NOTE: we need to make sure we never silently end up in an incorrect state.
 
         if block_to_unwind < self.init_block {
             todo!("Handle error")
         }
 
-        // We can unwrap here because we have checked that
-        // the block number is < latest block and > the oldest block
+        // If the block to unwind is greater than the latest state change in the block, exit early
+        if cache
+            .front()
+            .map_or(true, |latest| block_to_unwind >= latest.block_number)
+        {
+            return vec![];
+        }
+
         let pivot_idx = cache
             .iter()
-            .rev()
             .position(|state_change| state_change.block_number < block_to_unwind);
 
         let state_changes = if let Some(pivot_idx) = pivot_idx {
-            // Calculate the correct index from the front since we used `rev()`
-            // NOTE: explain why we arent using len - 1 and index + 1
-            // We need to drain the state changes including pivot + 1
-            let drain_idx = cache.len() - pivot_idx;
-
-            cache.drain(drain_idx..).collect()
+            cache.drain(..pivot_idx).collect()
         } else {
-            cache.drain(0..).collect()
+            cache.drain(..).collect::<Vec<StateChange>>()
         };
 
-        todo!("Need to flatten with rev precedence and then return the state changes")
+        self.flatten_state_changes(state_changes)
+    }
+
+    fn flatten_state_changes(&self, state_changes: Vec<StateChange>) -> Vec<AMM> {
+        state_changes
+            .into_iter()
+            .rev()
+            .fold(HashMap::new(), |mut amms, state_change| {
+                for amm in state_change.state_change {
+                    amms.entry(amm.address()).or_insert(amm);
+                }
+                amms
+            })
+            .into_values()
+            .collect()
     }
 }
