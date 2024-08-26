7bab07bb8df2cbc4efa7279118fb2e0039d1c424
0xKitsune <0xKitsune@protonmail.com>
2024-08-25 23:47:16 -0400

    update unwind_state_changes, added todos

--- src/state_space/cache.rs
+++ src/state_space/cache.rs
@@ -2,11 +2,19 @@ use super::StateChange;
 use arraydeque::{ArrayDeque, CapacityError};
 
 #[derive(Debug)]
-pub struct StateChangeCache(ArrayDeque<StateChange, 150>);
+
+pub struct StateChangeCache {
+    // TODO: add comment explaining why we need to store the initial block number
+    init_block: u64,
+    cache: ArrayDeque<StateChange, 150>,
+}
 
 impl StateChangeCache {
-    pub fn new() -> Self {
-        StateChangeCache(ArrayDeque::new())
+    pub fn new(init_block: u64) -> Self {
+        StateChangeCache {
+            init_block,
+            cache: ArrayDeque::new(),
+        }
     }
 
     //TODO: push back
@@ -19,23 +27,23 @@ impl StateChangeCache {
     }
 
     pub fn pop_front(&mut self) -> Option<StateChange> {
-        self.0.pop_front()
+        self.cache.pop_front()
     }
 
     pub fn capacity(&self) -> usize {
-        self.0.capacity()
+        self.cache.capacity()
     }
 
     pub fn len(&self) -> usize {
-        self.0.len()
+        self.cache.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
+        self.cache.is_empty()
     }
 
     pub fn clear(&mut self) {
-        self.0.clear();
+        self.cache.clear();
     }
 
     // TODO: add state change to cache
@@ -53,42 +61,37 @@ impl StateChangeCache {
         cache.push_front(state_change)
     }
 
-    async fn unwind_state_changes(
-        &mut self,
-        block_to_unwind: u64,
-    ) -> Result<(), CapacityError<StateChange>> {
-    }
-}
-/// Unwinds the state changes cache for every block from the most recent state change cache back to the block to unwind -1.
-// TODO: return vec of current state of amms after unwinding
-async fn unwind_state_changes(block_to_unwind: u64) -> Result<(), StateChangeError> {
-    let mut state_change_cache = state_change_cache.write().await;
+    // TODO: update return type
+    async fn unwind_state_changes(&mut self, block_to_unwind: u64) -> Vec<StateChange> {
+        let cache = &mut self.cache;
 
-    // TODO: iterate through state changes
-    // TODO: check the block number, if it is >= block to unwind, keep going until we are < block to unwind and then, flatten and drain the updates
+        // TODO: check if the block to unwind is greater than the most recent state change block number
+        // TODO: check if the block to unwind is less than the oldest state change block number,
+        // NOTE: this needs to handle cases where cache is empty or the state changes are newer than the unwind
+        // NOTE: we need to make sure we never silently end up in an incorrect state.
 
-    loop {
-        // check if the most recent state change block is >= the block to unwind,
-        if let Some(state_change) = state_change_cache.get(0) {
-            if state_change.block_number >= block_to_unwind {
-                if let Some(option_state_changes) = state_change_cache.pop_front() {
-                    if let Some(state_changes) = option_state_changes.state_change {
-                        for amm_state in state_changes {
-                            state.write().await.insert(amm_state.address(), amm_state);
-                        }
-                    }
-                } else {
-                    // We know that there is a state change from state_change_cache.get(0) so when we pop front without returning a value, there is an issue
-                    return Err(StateChangeError::PopFrontError);
-                }
-            } else {
-                return Ok(());
-            }
-        } else {
-            // We return an error here because we never want to be unwinding past where we have state changes.
-            // For example, if you initialize a state space that syncs to block 100, then immediately after there is a chain reorg to 95, we can not roll back the state
-            // changes for an accurate state space. In this case, we return an error
-            return Err(StateChangeError::NoStateChangesInCache);
+        if block_to_unwind < self.init_block {
+            todo!("Handle error")
         }
+
+        // We can unwrap here because we have checked that
+        // the block number is < latest block and > the oldest block
+        let pivot_idx = cache
+            .iter()
+            .rev()
+            .position(|state_change| state_change.block_number < block_to_unwind);
+
+        let state_changes = if let Some(pivot_idx) = pivot_idx {
+            // Calculate the correct index from the front since we used `rev()`
+            // NOTE: explain why we arent using len - 1 and index + 1
+            // We need to drain the state changes including pivot + 1
+            let drain_idx = cache.len() - pivot_idx;
+
+            cache.drain(drain_idx..).collect()
+        } else {
+            cache.drain(0..).collect()
+        };
+
+        todo!("Need to flatten with rev precedence and then return the state changes")
     }
 }
