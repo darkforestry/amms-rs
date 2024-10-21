0ee2be01a9d20fb6927c66dfca6d23aa42d1f94a
0xKitsune <0xkitsune@protonmail.com>
2024-10-20 22:08:05 -0400

    add sync logic for uv3 pool

--- src/amms/uniswap_v3/mod.rs
+++ src/amms/uniswap_v3/mod.rs
@@ -19,6 +19,7 @@ use std::{
     hash::{Hash, Hasher},
     sync::Arc,
 };
+use tracing::event;
 
 sol!(
 // UniswapV2Factory
@@ -123,7 +124,41 @@ impl AutomatedMarketMaker for UniswapV3Pool {
     }
 
     fn sync(&mut self, log: Log) {
-        todo!()
+        let event_signature = log.topics()[0];
+
+        if event_signature == IUniswapV3PoolEvents::Swap::SIGNATURE_HASH {
+            // TODO: we could use a feature flag to enable/disable validation
+            let swap_event =
+                IUniswapV3PoolEvents::Swap::decode_log(log.as_ref(), false).expect("TODO: ");
+
+            self.sqrt_price = swap_event.sqrtPriceX96.to();
+            self.liquidity = swap_event.liquidity;
+            self.tick = swap_event.tick.unchecked_into();
+
+            // tracing::debug!(?swap_event, address = ?self.address, sqrt_price = ?self.sqrt_price, liquidity = ?self.liquidity, tick = ?self.tick, "UniswapV3 swap event");
+        } else if event_signature == IUniswapV3PoolEvents::Mint::SIGNATURE_HASH {
+            let mint_event =
+                IUniswapV3PoolEvents::Mint::decode_log(log.as_ref(), false).expect("TODO: ");
+
+            self.modify_position(
+                mint_event.tickLower.unchecked_into(),
+                mint_event.tickUpper.unchecked_into(),
+                mint_event.amount as i128,
+            );
+            // tracing::debug!(?mint_event, address = ?self.address, sqrt_price = ?self.sqrt_price, liquidity = ?self.liquidity, tick = ?self.tick, "UniswapV3 mint event");
+        } else if event_signature == IUniswapV3PoolEvents::Burn::SIGNATURE_HASH {
+            let burn_event =
+                IUniswapV3PoolEvents::Burn::decode_log(log.as_ref(), false).expect("TODO: ");
+
+            self.modify_position(
+                burn_event.tickLower.unchecked_into(),
+                burn_event.tickUpper.unchecked_into(),
+                -(burn_event.amount as i128),
+            );
+            // tracing::debug!(?burn_event, address = ?self.address, sqrt_price = ?self.sqrt_price, liquidity = ?self.liquidity, tick = ?self.tick, "UniswapV3 burn event");
+        } else {
+            todo!("TODO: Handle this error")
+        }
     }
 
     fn simulate_swap(
@@ -150,6 +185,101 @@ impl AutomatedMarketMaker for UniswapV3Pool {
 
     fn calculate_price(&self, base_token: Address, quote_token: Address) -> Result<f64, AMMError> {
         todo!()
+    }
+}
+
+impl UniswapV3Pool {
+    /// Modifies a positions liquidity in the pool.
+    pub fn modify_position(&mut self, tick_lower: i32, tick_upper: i32, liquidity_delta: i128) {
+        //We are only using this function when a mint or burn event is emitted,
+        //therefore we do not need to checkTicks as that has happened before the event is emitted
+        self.update_position(tick_lower, tick_upper, liquidity_delta);
+
+        if liquidity_delta != 0 {
+            //if the tick is between the tick lower and tick upper, update the liquidity between the ticks
+            if self.tick > tick_lower && self.tick < tick_upper {
+                self.liquidity = if liquidity_delta < 0 {
+                    self.liquidity - ((-liquidity_delta) as u128)
+                } else {
+                    self.liquidity + (liquidity_delta as u128)
+                }
+            }
+        }
+    }
+
+    pub fn update_position(&mut self, tick_lower: i32, tick_upper: i32, liquidity_delta: i128) {
+        let mut flipped_lower = false;
+        let mut flipped_upper = false;
+
+        if liquidity_delta != 0 {
+            flipped_lower = self.update_tick(tick_lower, liquidity_delta, false);
+            flipped_upper = self.update_tick(tick_upper, liquidity_delta, true);
+            if flipped_lower {
+                self.flip_tick(tick_lower, self.tick_spacing);
+            }
+            if flipped_upper {
+                self.flip_tick(tick_upper, self.tick_spacing);
+            }
+        }
+
+        if liquidity_delta < 0 {
+            if flipped_lower {
+                self.ticks.remove(&tick_lower);
+            }
+
+            if flipped_upper {
+                self.ticks.remove(&tick_upper);
+            }
+        }
+    }
+
+    pub fn update_tick(&mut self, tick: i32, liquidity_delta: i128, upper: bool) -> bool {
+        let info = match self.ticks.get_mut(&tick) {
+            Some(info) => info,
+            None => {
+                self.ticks.insert(tick, Info::default());
+                self.ticks
+                    .get_mut(&tick)
+                    .expect("Tick does not exist in ticks")
+            }
+        };
+
+        let liquidity_gross_before = info.liquidity_gross;
+
+        let liquidity_gross_after = if liquidity_delta < 0 {
+            liquidity_gross_before - ((-liquidity_delta) as u128)
+        } else {
+            liquidity_gross_before + (liquidity_delta as u128)
+        };
+
+        // we do not need to check if liqudity_gross_after > maxLiquidity because we are only calling update tick on a burn or mint log.
+        // this should already be validated when a log is
+        let flipped = (liquidity_gross_after == 0) != (liquidity_gross_before == 0);
+
+        if liquidity_gross_before == 0 {
+            info.initialized = true;
+        }
+
+        info.liquidity_gross = liquidity_gross_after;
+
+        info.liquidity_net = if upper {
+            info.liquidity_net - liquidity_delta
+        } else {
+            info.liquidity_net + liquidity_delta
+        };
+
+        flipped
+    }
+
+    pub fn flip_tick(&mut self, tick: i32, tick_spacing: i32) {
+        let (word_pos, bit_pos) = uniswap_v3_math::tick_bitmap::position(tick / tick_spacing);
+        let mask = U256::from(1) << bit_pos;
+
+        if let Some(word) = self.tick_bitmap.get_mut(&word_pos) {
+            *word ^= mask;
+        } else {
+            self.tick_bitmap.insert(word_pos, mask);
+        }
     }
 }
 
@@ -187,7 +317,7 @@ impl AutomatedMarketMakerFactory for UniswapV3Factory {
 
     fn create_pool(&self, log: Log) -> Result<AMM, AMMError> {
         let pool_created_event =
-            IUniswapV3Factory::PoolCreated::decode_log(&log.inner, true).expect("TODO:");
+            IUniswapV3Factory::PoolCreated::decode_log(&log.inner, false).expect("TODO:");
 
         Ok(AMM::UniswapV3Pool(UniswapV3Pool {
             address: pool_created_event.pool,
