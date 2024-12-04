60dcb0395c72b86d2e019477c298f9ed6c1c5c49
0xKitsune <0xkitsune@protonmail.com>
2024-12-03 23:27:47 -0500

    update calculate price

--- src/amms/erc_4626/mod.rs
+++ src/amms/erc_4626/mod.rs
@@ -1,10 +1,17 @@
+use std::cmp::Ordering;
+
 use alloy::{
     primitives::{Address, B256, U256},
     rpc::types::Log,
 };
 use serde::{Deserialize, Serialize};
 
-use super::{amm::AutomatedMarketMaker, consts::U256_10000, error::AMMError};
+use super::{
+    amm::AutomatedMarketMaker,
+    consts::{U128_0X10000000000000000, U256_10000},
+    error::AMMError,
+    uniswap_v2::{div_uu, q64_to_float},
+};
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct ERC4626Vault {
@@ -41,8 +48,8 @@ impl AutomatedMarketMaker for ERC4626Vault {
         vec![self.vault_token, self.asset_token]
     }
 
-    fn calculate_price(&self, base_token: Address, quote_token: Address) -> Result<f64, AMMError> {
-        todo!()
+    fn calculate_price(&self, base_token: Address, _quote_token: Address) -> Result<f64, AMMError> {
+        Ok(q64_to_float(self.calculate_price_64_x_64(base_token)?)?)
     }
 
     fn simulate_swap(
@@ -87,5 +94,38 @@ impl ERC4626Vault {
         };
 
         amount_in * reserve_out / reserve_in * U256::from(10000 - fee) / U256_10000
+    }
+
+    // TODO: Right now this will return a uv2 error, fix this
+    pub fn calculate_price_64_x_64(&self, base_token: Address) -> Result<u128, AMMError> {
+        let decimal_shift = self.vault_token_decimals as i8 - self.asset_token_decimals as i8;
+
+        // Normalize reserves by decimal shift
+        let (r_v, r_a) = match decimal_shift.cmp(&0) {
+            Ordering::Less => (
+                self.vault_reserve * U256::from(10u128.pow(decimal_shift.unsigned_abs() as u32)),
+                self.asset_reserve,
+            ),
+            _ => (
+                self.vault_reserve,
+                self.asset_reserve * U256::from(10u128.pow(decimal_shift as u32)),
+            ),
+        };
+
+        // Withdraw
+        if base_token == self.vault_token {
+            if r_v.is_zero() {
+                // Return 1 in Q64
+                Ok(U128_0X10000000000000000)
+            } else {
+                Ok(div_uu(r_a, r_v)?)
+            }
+        // Deposit
+        } else if r_a.is_zero() {
+            // Return 1 in Q64
+            Ok(U128_0X10000000000000000)
+        } else {
+            Ok(div_uu(r_v, r_a)?)
+        }
     }
 }
